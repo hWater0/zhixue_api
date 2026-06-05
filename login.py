a03c9ae7a22f03721995e8fa19441ef0fd108b87
Kevin-100717 <kevin100717@foxmail.com>
2026-06-05 17:18:27 +0800

    存档

--- login.py
+++ login.py
@@ -6,8 +6,8 @@ import binascii
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5
 import base64
-USERNAME = "84882682"
-PASSWORD = "qwer1234"
+USERNAME = "你的号"
+PASSWORD = "你的密码"
 headers = {
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36 Edg/148.0.0.0"
 }
